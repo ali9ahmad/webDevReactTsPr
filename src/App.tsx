6b9f562eaac869b03a19cc22f4427ca1fe341b8f
git <temur.khan.dispatch@gmail.com>
2023-11-09 22:03:35 +0500

    enough for today

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
