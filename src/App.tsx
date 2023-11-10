af5421e3fe2998f75f058dd1f61d532ffd2b4017
git <temur.khan.dispatch@gmail.com>
2023-11-10 15:39:42 +0500

    reactbootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { NewNote } from './Components/NewNote';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
