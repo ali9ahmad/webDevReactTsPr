973359fa1796346135e399628822502ae59d6c79
git <temur.khan.dispatch@gmail.com>
2023-11-13 16:45:39 +0500

    saving the work

--- src/Components/NoteList.tsx
+++ src/Components/NoteList.tsx
@@ -1,9 +1,15 @@
 import { useMemo, useState } from 'react';
-import { Badge, Button, Card, Col, Form, Row, Stack } from 'react-bootstrap';
+import { Badge, Button, Card, Col, Form, Modal, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import ReactSelect from 'react-select';
 import { Tag } from '../App';
 import styles from '../Modules/NoteList.module.css';
+
+type EditTagsModalProps = {
+  show: boolean;
+  available: Tag[];
+  handleClose: () => void;
+};
 
 type SimplifiedNote = {
   tags: Tag[];
@@ -19,6 +25,7 @@ type NoteListProps = {
 export function NoteList({ availableTags, notes }: NoteListProps) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState('');
+  const [editTagsModalIsOpen, seteditTagsModalIsOpen] = useStete(false);
 
   const filteredNotes = useMemo(() => {
     return notes.filter((note) => {
@@ -83,6 +90,7 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
           </Col>
         ))}
       </Row>
+      <EditTagsModal show={} handeClose={} availableTags={availableTags} />
     </>
   );
 }
@@ -108,5 +116,31 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
         </Stack>
       </Card.Body>
     </Card>
+  );
+}
+
+function EditTagsModal({ availableTags, handleClose, show }: EditTagsModalProps) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Tags</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Stack gap={2}>
+            {availableTags.map((tag) => (
+              <Row key={tag.id}>
+                <Col>
+                  <Form.Control type="text" value={tag.label} />
+                </Col>
+                <Col xs="auto">
+                  <Button variant="outline-danger">&times;</Button>
+                </Col>
+              </Row>
+            ))}
+          </Stack>
+        </Form>
+      </Modal.Body>
+    </Modal>
   );
 }
